123a68fdee939e566b791134840207a2d9b2b1ec
Adrian PPG <adrianppg3@yahoo.es>
2012-03-27 20:56:40 +0000

    commit to heroku

--- to_heroku_cucumber.sh
+++ to_heroku_cucumber.sh
@@ -2,7 +2,7 @@ tar czf hw4.tar.gz app/ config/ db/migrate features/ spec/ Gemfile
 git add .
 git commit -m "$1" -a
 git push heroku master
-#bundle exec cucumber features
+bundle exec cucumber features
 if echo $2 | grep "git"; then
   git push origin master &
 fi
