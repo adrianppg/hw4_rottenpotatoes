6297d067edd630fdc7ada29792ccaec46f7ec004
Adrian PPG <adrianppg3@yahoo.es>
2012-03-29 08:00:00 +0000

    +++5

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -24,7 +24,7 @@ module NavigationHelpers
     when /^the edit page for \"(.*)\"$/i
       edit_movie_path(Movie.find_by_title($1))
     when /^the details page for \"(.*)\"$/i
-      show_movie_path(Movie.find_by_title($1))
+      movie_path(Movie.find_by_title($1))
     else
       begin
         page_name =~ /^the (.*) page$/
